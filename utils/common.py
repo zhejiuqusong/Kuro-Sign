2cfb7cbd9eb7fa2a90eab3f13924648ec9ec2f06
Night-stars-1 <nujj1042633805@gmail.com>
2024-10-27 17:38:01 +0800

    fix: 环境变量使用下划线

--- utils/common.py
+++ utils/common.py
@@ -1,3 +1,9 @@
+"""
+Author: Night-stars-1 nujj1042633805@gmail.com
+Date: 2024-10-27 15:38:10
+LastEditors: Night-stars-1 nujj1042633805@gmail.com
+LastEditTime: 2024-10-27 17:37:53
+"""
 from datetime import datetime
 import json
 import os
@@ -25,7 +31,7 @@ def read_config():
             return json.load(f)
     raise FileNotFoundError("请将config.json.example重命名为config.json并填写信息")
 
-if not (token := os.environ.get("Kuro-Token")):
+if not (token := os.environ.get("Kuro_Token")):
     token = read_config()["token"]
 
 headers = {
