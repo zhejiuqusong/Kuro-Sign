59aaf44c939f75c3a246e16ececdc6c33e2a37b4
Night-stars-1 <nujj1042633805@gmail.com>
2025-02-04 23:59:06 +0800

    feat: 适配多账号

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ from utils.bbs_sign import (
     like,
     share,
 )
+from utils.common import get_token_list, set_token
 from utils.game_sign import get_role_list
 
 GAME_LIST = [2, 3]
@@ -34,30 +35,33 @@ def log(str):
 
 
 if __name__ == "__main__":
-    for game_id in GAME_LIST:
-        sign_list = get_role_list(game_id)
-        if isinstance(sign_list, str):
-            raise ValueError(sign_list)
-        for sign_obj in sign_list:
-            sign_status = sign_obj.sign()
-            log(f"{sign_obj.game_name}签到：{sign_status}")
-    task_list = get_task_list()
-    forum_list = get_forum_list()
-    for task in task_list:
-        time = task["needActionTimes"] - task["completeTimes"]
-        for i in range(time):
-            if task["remark"] == "用户签到":
-                sign_status = bbs_sign(random.choice(GAME_LIST))
-                log(f"用户签到：{sign_status}")
-            elif task["remark"] == "浏览3篇帖子":
-                title = get_forum_detail(forum_list[i]["postId"])
-                log(f"阅读帖子：{title}")
-            elif task["remark"] == "点赞5次":
-                like_status = like(forum_list[i]["postId"], forum_list[i]["userId"])
-                log(f"点赞帖子：{forum_list[i]['postTitle']} - {like_status}")
-            elif task["remark"] == "分享1次帖子":
-                share_status = share()
-                log(f"分享帖子：{share_status}")
-        if time == 0:
-            log(f"{task['remark']}：已完成")
+    token_list = get_token_list()
+    for token in token_list:
+        set_token(token)
+        for game_id in GAME_LIST:
+            sign_list = get_role_list(game_id)
+            if isinstance(sign_list, str):
+                raise ValueError(sign_list)
+            for sign_obj in sign_list:
+                sign_status = sign_obj.sign()
+                log(f"{sign_obj.game_name}签到：{sign_status}")
+        task_list = get_task_list()
+        forum_list = get_forum_list()
+        for task in task_list:
+            time = task["needActionTimes"] - task["completeTimes"]
+            for i in range(time):
+                if task["remark"] == "用户签到":
+                    sign_status = bbs_sign(random.choice(GAME_LIST))
+                    log(f"用户签到：{sign_status}")
+                elif task["remark"] == "浏览3篇帖子":
+                    title = get_forum_detail(forum_list[i]["postId"])
+                    log(f"阅读帖子：{title}")
+                elif task["remark"] == "点赞5次":
+                    like_status = like(forum_list[i]["postId"], forum_list[i]["userId"])
+                    log(f"点赞帖子：{forum_list[i]['postTitle']} - {like_status}")
+                elif task["remark"] == "分享1次帖子":
+                    share_status = share()
+                    log(f"分享帖子：{share_status}")
+            if time == 0:
+                log(f"{task['remark']}：已完成")
     send("库街区", "\n".join(MESSAGE))
--- utils/common.py
+++ utils/common.py
@@ -2,13 +2,14 @@
 Author: Night-stars-1 nujj1042633805@gmail.com
 Date: 2024-10-27 15:38:10
 LastEditors: Night-stars-1 nujj1042633805@gmail.com
-LastEditTime: 2024-10-29 01:19:19
+LastEditTime: 2025-02-04 23:57:28
 """
-from datetime import datetime
+
 import json
 import os
 import random
 import string
+from datetime import datetime
 from typing import TypedDict
 
 
@@ -17,13 +18,16 @@ class ResponseModel(TypedDict):
     msg: str
     data: list
 
+
 def random_string(length: int):
     letters = string.ascii_letters
-    return ''.join(random.choice(letters) for _ in range(length))
+    return "".join(random.choice(letters) for _ in range(length))
+
 
 def current_month():
     now = datetime.now()
     return now.strftime("%m")
+
 
 def read_config():
     if os.path.exists("config.json"):
@@ -31,8 +35,24 @@ def read_config():
             return json.load(f)
     raise FileNotFoundError("请将config.json.example重命名为config.json并填写信息")
 
-if not (token := os.environ.get("Kuro_Token")):
-    token = read_config()["token"]
+
+def get_token_list():
+    """
+    获取token列表
+    """
+    if not (token := os.environ.get("Kuro_Token")):
+        data: dict[str, str] = read_config()
+        token = data["token"]
+
+    return token.split("|")
+
+
+def set_token(token: str):
+    """
+    设置headers 的 token
+    """
+    headers["token"] = token
+
 
 headers = {
     "User-Agent": "okhttp/3.11.0",
@@ -41,5 +61,4 @@ headers = {
     "source": "android",
     "version": "2.2.5",
     "versionCode": "2250",
-    "token": token,
 }
